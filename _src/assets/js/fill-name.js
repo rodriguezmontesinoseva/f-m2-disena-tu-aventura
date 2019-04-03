1bb6ef2685f0333ebe7a13c24e81c904c6a037ce
carmen-tm <carmentorremolina@gmail.com>
2019-04-03 20:59:47 +0200

    Initial commit with js-linkedin

---
+++ _src/assets/js/fill-name.js
@@ -0,0 +1,34 @@
+// 'use strict';
+
+// console.log('>> fill-name running :)');
+
+// // Element listener
+// const nameInputEl = document.querySelector("#fullName");
+// // Element target
+// const cardNameEl = document.querySelector(".full-name__card ");
+
+
+// // Photo of default value:
+// const defaultName = cardNameEl.innerHTML;
+
+
+// //Handler
+// function printName() {
+//     console.log("handler is working");
+
+//     // if (nameInputEl.value === "") {
+//     if (!nameInputEl.value) {
+//         cardNameEl.innerHTML = defaultName;
+
+//     } else {
+//         cardNameEl.innerHTML = nameInputEl.value;
+//     }
+// }
+
+// //Handler2
+// function printName3 () {
+//     cardNameEl.innerHTML = nameInputEl.value || defaultName;
+// }
+
+// //Listener
+// nameInputEl.addEventListener("keyup", printName2);
