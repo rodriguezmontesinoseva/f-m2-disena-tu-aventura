f931bfe05b5c93ec86c35d6aaebf1becf8a4c53e
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-04-03 20:38:23 +0200

    test-js-Eva

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputEl = document.querySelector(".item__info");
+const nbreTarjeta = document.querySelector (".full-name__card");
+console.log(nbreTarjeta);
+const nbreTarjetaIni = nbreTarjeta.innerHTML;
+console.log(nbreTarjetaIni);
+
+function actualizaDatos(){
+    // if (inputEl.value ===""){ //  !inputEl.value
+    //   nbreTarjeta.innerHTML = nbreTarjetaIni;
+    // }else{
+    //     nbreTarjeta.innerHTML = inputEl.value;
+    // }
+
+    nbreTarjeta.innerHTML = inputEl.value  || nbreTarjetaIni; //inputEl.value al ponerlo en el primer termino del OR || ; mira si hay algo en inputEl, si no hay nada devuelve False y coge el segundo término.
+}
+
+
+inputEl.addEventListener('keyup',actualizaDatos);
