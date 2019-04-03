864c6e6f80afa85e26e27d7875be224a9e88feec
carmen-tm <carmentorremolina@gmail.com>
2019-04-03 20:10:45 +0200

    Test card name interactivity

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+// Element listener
+const nameInputEl = document.querySelector("#fullName");
+// Element target
+const cardNameEl = document.querySelector(".full-name__card ");
+
+
+// Photo of default value:
+const defaultName = cardNameEl.innerHTML;
+
+
+//Handler
+function printName() {
+    console.log("handler is working");
+
+    // if (nameInputEl.value === "") {
+    if (!nameInputEl.value) {
+        cardNameEl.innerHTML = defaultName;
+
+    } else {
+        cardNameEl.innerHTML = nameInputEl.value;
+    }
+}
+
+//Handler2
+function printName3 () {
+    cardNameEl.innerHTML = nameInputEl.value || defaultName;
+}
+
+//Listener
+nameInputEl.addEventListener("keyup", printName2);
+
