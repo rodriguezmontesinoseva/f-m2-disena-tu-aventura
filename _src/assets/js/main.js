c8a4c1991fc6d5d95f88f93608f401a87e188a77
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-04-04 20:29:39 +0200

    add js change class colour

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,20 +3,29 @@
 const option1ColourEl = document.querySelector('#option-color-1');
 const option2ColourEl = document.querySelector('#option-color-2');
 const option3ColourEl = document.querySelector('#option-color-3');
+//me traigo el objeto con clase .preview__card que contiene la previsualización
+const previewCardEl = document.querySelector('.preview__card');
 
 
 console.log(option1ColourEl);
 console.log(option2ColourEl);
 console.log(option3ColourEl);
+console.log(previewCardEl);
 
 function changeColourPreview(){
 
     if (option1ColourEl.checked){
+        //meto la clase theme-green al div class: preview__card
+        //cuando funcione ver si le puedo poner toggle
+        previewCardEl.classList.add('theme-green');
         console.log('option1 marcada');
+
     }else if(option2ColourEl.checked){
+        previewCardEl.classList.add('theme-red');
         console.log('option2 marcada');
 
     }else if(option3ColourEl.checked){
+        previewCardEl.classList.add('theme-blue');
         console.log('option3 marcada');
 
     }
