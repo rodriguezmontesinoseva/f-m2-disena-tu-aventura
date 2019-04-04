226ea1217679dd1bc2172bd408c4a6ed829dbaeb
carmen-tm <carmentorremolina@gmail.com>
2019-04-04 15:18:07 +0200

    Integrate the fill-name interaction with preview

--- _src/assets/js/fill-name.js
+++ _src/assets/js/fill-name.js
@@ -1,34 +1,34 @@
-// 'use strict';
+'use strict';
 
-// console.log('>> fill-name running :)');
+console.log('>> fill-name running :)');
 
-// // Element listener
-// const nameInputEl = document.querySelector("#fullName");
-// // Element target
-// const cardNameEl = document.querySelector(".full-name__card ");
-
-
-// // Photo of default value:
-// const defaultName = cardNameEl.innerHTML;
+// Element listener
+const nameInputEl = document.querySelector("#fullName");
+// Element target
+const cardNameEl = document.querySelector(".full-name__card ");
 
 
-// //Handler
-// function printName() {
-//     console.log("handler is working");
+// Photo of default value:
+const defaultName = cardNameEl.innerHTML;
 
-//     // if (nameInputEl.value === "") {
-//     if (!nameInputEl.value) {
-//         cardNameEl.innerHTML = defaultName;
 
-//     } else {
-//         cardNameEl.innerHTML = nameInputEl.value;
-//     }
-// }
+//Handler
+function printName() {
+    console.log("handler is working");
 
-// //Handler2
-// function printName3 () {
-//     cardNameEl.innerHTML = nameInputEl.value || defaultName;
-// }
+    // if (nameInputEl.value === "") {
+    if (!nameInputEl.value) {
+        cardNameEl.innerHTML = defaultName;
 
-// //Listener
-// nameInputEl.addEventListener("keyup", printName2);
+    } else {
+        cardNameEl.innerHTML = nameInputEl.value;
+    }
+}
+
+//Handler2
+function printName2 () {
+    cardNameEl.innerHTML = nameInputEl.value || defaultName;
+}
+
+//Listener
+nameInputEl.addEventListener("keyup", printName2);
