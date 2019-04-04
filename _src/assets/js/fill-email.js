8722c334c37038fcd63ba195d3a66c72cb98352b
marinarodriguezandres <marinarodriguezandres@gmail.com>
2019-04-04 16:16:41 +0200

    Add email function

---
+++ _src/assets/js/fill-email.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// EMAIL //
+
+const defectEmailPreview = document.querySelector('.email__card').innerHTML;
+const emailLink = document.querySelector('#email');
+const emailPreviewLink = document.querySelector('.email__card');
+
+
+function changeEmailOnCard (){
+    const emailHref = emailLink.value;
+    // Si no hay e-mail, el href no se genera //
+    if (!emailHref){
+    emailPreviewLink.removeAttribute("href");
+    }
+    // Si lo hay, se crea //
+    else {
+    emailPreviewLink.href = `mailto:${emailHref}`;
+    }
+}
+emailLink.addEventListener('keyup', changeEmailOnCard);
