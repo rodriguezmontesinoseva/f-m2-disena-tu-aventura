64b1cc40861252e868bf084a0da59831c3c861a1
carmen-tm <carmentorremolina@gmail.com>
2019-04-04 16:18:50 +0200

    Improve html structure and js

--- _src/assets/js/fill-linkedin.js
+++ _src/assets/js/fill-linkedin.js
@@ -6,31 +6,27 @@ console.log('>> fill-linkedin running :)');
 const linkedinInputEl = document.querySelector("#linkedin");
 
 // Element target
-const linkedinCardEl = document.querySelector(".linkedin-icon ");
+const linkedinCardEl = document.querySelector(".linkedin-link");
 
 // Default value for linkedin:
 const linkedinDefault = "#";
 
 //Handler:
 function linkBottonLinkedin1 () {
-    linkedinCardEl.innerHTML = `
-    <a href="${linkedinInputEl.value}" target="_blank"> 
-        <img class="wrap-icon" src="assets/images/linkedin.svg" alt="linkedin">
-    </a>
-    `
+    linkedinCardEl.href = linkedinInputEl.value;
 };
 
-function linkBottonLinkedin2 () {
-    linkedinCardEl.innerHTML = `
-    <a href="${linkedinInputEl.value}" target="_blank"> 
-        <img class="wrap-icon" src="assets/images/linkedin.svg" alt="linkedin">
-    </a>
-    ` || `
-    <a href="${linkedinDefault}" target="_blank"> 
-        <img class="wrap-icon" src="assets/images/linkedin.svg" alt="linkedin">
-    </a>
-    `
-}
+// function linkBottonLinkedin2 () {
+//     linkedinCardEl.innerHTML = `
+//     <a href="${linkedinInputEl.value}" target="_blank"> 
+//         <img class="wrap-icon" src="assets/images/linkedin.svg" alt="linkedin">
+//     </a>
+//     ` || `
+//     <a href="${linkedinDefault}" target="_blank"> 
+//         <img class="wrap-icon" src="assets/images/linkedin.svg" alt="linkedin">
+//     </a>
+//     `
+// }
 
 //Listener. I have choosen a "change" event so it just interacts when the full url is filled.
 linkedinInputEl.addEventListener("change",linkBottonLinkedin1)
