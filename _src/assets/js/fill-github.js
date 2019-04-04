e5052f7c4c0e248668ba348381abd480b291943c
irenegf83 <irene@irenegf.com>
2019-04-04 16:12:50 +0200

    Add safe link in icon github

--- _src/assets/js/fill-github.js
+++ _src/assets/js/fill-github.js
@@ -1,13 +1,20 @@
 'use strict';
 
 const githubEl = document.querySelector('#github');
-const githubLink = document.querySelector('.link-github');
+const githubLinkEl = document.querySelector('.link-github');
+let githubHrefIcon = githubLinkEl.href; // "Foto" del contenido que trae por defecto el href
+
 
 function putLinkIcon() {
-    const userGitHub = githubEl.nodeValue;
-    console.log(userGitHub);
+    const githubUser = githubEl.value; // valor introducido en el input
+    const githubDefinitiveLink =  githubHrefIcon + githubUser; // valor final del link
+    const githubValueDefault = 'https://github.com/';
+
+    githubHrefIcon = githubDefinitiveLink || githubValueDefault;
+
+
+    console.log(githubHrefIcon);
     
 }
 
 githubEl.addEventListener('keyup', putLinkIcon);
-// console.log(githubLink);
