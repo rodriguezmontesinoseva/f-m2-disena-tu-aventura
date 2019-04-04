5e667a3d1491739ab458e29b9b0edbabc7ac655e
carmen-tm <carmentorremolina@gmail.com>
2019-04-04 20:57:43 +0200

    Write the js logics of collapsible

---
+++ _src/assets/js/collapsible.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log("collapsible runningggg");
+
+//Coger elementos arrow
+
+//Listener en arrows del tipo "click"
+
+//Function tipo toogle / or / if else
+    //si estás oculto
+        //muéstra panel (añadir clase show)
+        //arrow top (default)
+    //si estás visible 
+        // ocúlta panel (añadir clase hidden)
+        // arrow botton (rotate 180º)
