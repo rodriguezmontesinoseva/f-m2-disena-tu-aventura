104fa7dab8a78797f3e6851773286f4a18ccbb2a
carmen-tm <carmentorremolina@gmail.com>
2019-04-05 13:50:46 +0200

    Improve the event listener

--- _src/assets/js/fill-linkedin.js
+++ _src/assets/js/fill-linkedin.js
@@ -3,19 +3,21 @@
 console.log('>> fill-linkedin running :)');
 
 // Element listener
-const linkedinInputEl = document.querySelector("#linkedin");
+const linkedinInputEl = document.querySelector('#linkedin');
 
 // Element target
-const linkedinCardEl = document.querySelector(".linkedin-link");
+const linkedinCardEl = document.querySelector('.linkedin-link');
 
 // Default value for linkedin:
 const linkedinDefault = linkedinCardEl.href;
 
 
 //Handler:
-function linkBottonLinkedin1 () {
+function linkBottonLinkedin () {
+    console.log(linkedinInputEl.value);
+
     linkedinCardEl.href = linkedinInputEl.value || linkedinDefault;
 };
 
-//Listener. I have choosen a "change" event so it just interacts when the full url is filled.
-linkedinInputEl.addEventListener("change",linkBottonLinkedin1)
+//Listener for the linkedin input element
+linkedinInputEl.addEventListener('keyup',linkBottonLinkedin)
