469fb23ed1ee5eeb1e6efb8c084286a6f9934e0a
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-04-06 13:22:52 +0200

    add partials svg icons preview

--- _src/assets/js/fill-colours.js
+++ _src/assets/js/fill-colours.js
@@ -1,60 +1,31 @@
 'use strict';
+//A color theme is selected in the DESIGN section and the preview is updated with those colors in real time.
 
 const option1ColourEl = document.querySelector('#option-color-1');
 const option2ColourEl = document.querySelector('#option-color-2');
 const option3ColourEl = document.querySelector('#option-color-3');
-//me traigo el objeto con clase .preview__card que contiene la previsualización
+//I bring the object with class .preview__card containing the preview
 const previewCardEl = document.querySelector('.preview__card');
 
-
-console.log(option1ColourEl);
-console.log(option2ColourEl);
-console.log(option3ColourEl);
-console.log(previewCardEl);
-
+//function asking which option-button is marked and loads that range of colors into the preview
 function changeColourPreview(){
-// probar con un switch-select
     if (option1ColourEl.checked){
-        //meto la clase theme-green al div class: preview__card
-        //cuando funcione ver si le puedo poner toggle
-        //intentar hacerlo con un metodo
-        previewCardEl.setAttribute('class', 'preview__card theme-green');
-        // previewCardEl.classList.remove('theme-red');
-        // previewCardEl.classList.remove('theme-blue');
-
-        // previewCardEl.classList.add('theme-green');
-        console.log('option1 marcada');
-        console.log(previewCardEl);
+         previewCardEl.classList.remove('theme-red');
+         previewCardEl.classList.remove('theme-blue');
+         previewCardEl.classList.add('theme-green');
 
     }else if(option2ColourEl.checked){
-        previewCardEl.setAttribute('class', 'preview__card theme-red');
-
-        // previewCardEl.classList.remove('theme-green');
-        // previewCardEl.classList.remove('theme-blue');
-
-        // previewCardEl.classList.add('theme-red');
-        console.log(previewCardEl);
-
-
-        console.log('option2 marcada');
+         previewCardEl.classList.remove('theme-green');
+         previewCardEl.classList.remove('theme-blue');
+         previewCardEl.classList.add('theme-red');
 
     }else if(option3ColourEl.checked){
-        previewCardEl.setAttribute('class', 'preview__card theme-blue');
-        // meter preview__card theme- en una variable;
-
-        // previewCardEl.classList.remove('theme-red');
-        // previewCardEl.classList.remove('theme-green');
-
-        // previewCardEl.classList.add('theme-blue');
-        console.log('option3 marcada');
-        console.log(previewCardEl);
-
-
+         previewCardEl.classList.remove('theme-red');
+         previewCardEl.classList.remove('theme-green');
+         previewCardEl.classList.add('theme-blue');
     }
 }
 
 option1ColourEl.addEventListener ('click', changeColourPreview);
 option2ColourEl.addEventListener ('click', changeColourPreview);
 option3ColourEl.addEventListener ('click', changeColourPreview);
-
-console.log('>> Ready main.js :)');
