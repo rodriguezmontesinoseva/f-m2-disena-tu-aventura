9298defa9e143dfcab9c778a7f62a325f1a9a6c5
carmen-tm <carmentorremolina@gmail.com>
2019-04-07 18:14:23 +0200

    Add collapsible show/hide behaviour to panels working

--- _src/assets/js/collapsible.js
+++ _src/assets/js/collapsible.js
@@ -8,29 +8,43 @@ console.log("collapsible runningggg");
 const arrowElDesign = document.getElementById("dropdown__down-arrow--design");
 const arrowElFill = document.getElementById("dropdown__down-arrow--fill");
 
+const panelDesignEl = document.querySelector(".panel-design__radios");
+const panelFillEl = document.querySelector(".panel-fill__form");
+
 
 //Handler function
 function collapsibleDesign() {
     console.log("click!");
     
     // arrow botton (rotate 180º)
-    arrowElDesign.classList.toggle("dropdown__down-arrow--rotated");
-}
+    arrowElDesign.classList.toggle("dropdown__arrow--up");
 
-function collapsibleFill() {
-    console.log("click!");
-    
-    // arrow botton (rotate 180º)
-    arrowElFill.classList.toggle("dropdown__down-arrow--rotated");
-}
+    arrowElDesign.classList.toggle("dropdown__arrow--down");
 
-//Function tipo toogle / or / if else
+    panelDesignEl.classList.toggle("panel--close"); 
+
+    //Function tipo toogle / or / if else
 //si estás oculto
 //muéstra panel (añadir clase show)
 //arrow top (default)
 //si estás visible 
 // ocúlta panel (añadir clase hidden)
 // arrow botton (rotate 180º)
+
+}
+
+function collapsibleFill() {
+    console.log("click!");
+    
+    // arrow botton (rotate 180º)
+    arrowElFill.classList.toggle("dropdown__arrow--up");
+
+    arrowElFill.classList.toggle("dropdown__arrow--down");
+
+    panelFillEl.classList.toggle("panel--close"); 
+}
+
+
 
 //Lintener in arrows ("click")
 arrowElDesign.addEventListener("click", collapsibleDesign);
