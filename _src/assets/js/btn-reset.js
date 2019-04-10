8baeb836d566b2142209eed287af326c3613de3a
irenegf83 <irene@irenegf.com>
2019-04-10 20:54:36 +0200

    Reset preview card name and job

--- _src/assets/js/btn-reset.js
+++ _src/assets/js/btn-reset.js
@@ -3,13 +3,20 @@ const btnResetEl = document.querySelector('.reset__button');
 // Cojemos todos los input
 const inputsEl = document.querySelectorAll('.item__info');
 const radioButtonEl = document.querySelector('#option-color-1');
-
+const nameEl = document.querySelector('.full-name__card');
 
 function handleButtonClick() {
     // Vaciar el valor de todos los input del formulario
     for(const input of inputsEl) {
         input.value = '';        
     }
+    printName2();
+    changeRolOnCard();
+    changeEmailOnCard();
+    phonenumber();
+    putLinkIcon();
+    linkBottonLinkedin();
+
     // Poner el color seleccionado en el default (primer radio button)
     radioButtonEl.click();
     // Borrar toda la info del caché
