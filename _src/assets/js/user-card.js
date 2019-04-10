eac2c40d7a3959dc7d60834db9c896ad5ac1ae83
carmen-tm <carmentorremolina@gmail.com>
2019-04-10 20:17:38 +0200

    Create emtpy object and example filled

--- _src/assets/js/user-card.js
+++ _src/assets/js/user-card.js
@@ -1,4 +1,26 @@
-//Create an empty object:
+//Create an empty object, following Adalab documentation instructions here
+// https://books.adalab.es/materiales-front-end-f/proyectos/p2_anexo
+
 const userData = {
-    
+    //from design, a number between 1-3, that relates with the 3 colour palettes
+    palette: "",
+    name: "",
+    job: "",
+    phone: "",
+    email: "",
+    linkedin: "",
+    GitHub: "",
+    photo: "",
+}
+
+//Example of the object filled with the user data
+const ExUserData = {
+    palette: 1,
+    name: "Monster Terribilus",
+    job: "Front-End",
+    phone: "+34666666",
+    email: "dontBeScare@gmail.com",
+    linkedin: "linkedin.com/monsterYey",
+    GitHub: "monsterYey",
+    photo: "data:image/png;base64,2342ba...",
 }
