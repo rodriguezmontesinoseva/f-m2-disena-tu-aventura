f61f8bd246362933e0d8fd5079ef30e82f8561b9
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-04-17 19:13:02 +0200

    add esLint

--- _src/assets/js/share-twitter.js
+++ _src/assets/js/share-twitter.js
@@ -1,63 +1,17 @@
-"use strict";
+'use strict';
 
-//al hacer clic en el botón compartir. muestra la url y boton compartir en twitter (ok)
-// mostrar la url: 
-// La URL base del servicio es
-// https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/
-// GET: una petición 'GET' a la URL /card/:id:/ devuelve una página web en HTML donde se muestran los datos de la tarjeta con el identificador 'id'
-//https://awesome-profile-card.com?id=A456DF0001
-
-//mostrar la url que devuelve la API, con la tarjeta de visita con la info rellena
-//COMPARTIR TWITTER - enlaza a twitter de la persona (usar campo usuario twitter) donde hay tweet con texto predefinido que incluye la url de la tarjeta
-
-// al pulsar compartir en twitter. se genera twit
-
-// {/* <a class="twitter-share-button"
-//   href="https://twitter.com/intent/tweet?text=Hello%20world">
-// Tweet</a> */}
-
-//fetch para pedir la url
-//meter esa url en el href
+//urlBack es el response del fetch que se hace hacia back. Cuando esté terminada la parte de enviar a back, cambiar el parámetro contenido en la llamada a la funcion "http://prueba-direccion-backend.html"
 
 function createLinkTweet(urlBack){
-    //cojo p donde irá el link
-    const cardLinkEl = document.querySelector('.card-link')
-    //href de donde ira el link = al link que nos mandan de back
-    cardLinkEl.href = urlBack;
-    //poner url de back entre ><
-    cardLinkEl.innerHTML = urlBack;
+  const cardLinkEl = document.querySelector('.card-link');
+  cardLinkEl.href = urlBack;
+  cardLinkEl.innerHTML = urlBack;
 
-    //cojo boton twitter
-    const cardLinkTweetEl = document.querySelector('.twitter-share-button');
-    //href=href + card-link
-    cardLinkTweetEl.href = cardLinkTweetEl.href + urlBack;
+  const cardLinkTweetEl = document.querySelector('.twitter-share-button');
+  cardLinkTweetEl.href = cardLinkTweetEl.href + urlBack;
 
 }
 
-createLinkTweet("http://prueba-direccion-backend.html");
+createLinkTweet('http://prueba-direccion-backend.html');
 
 
-// const linkCardMContent = linkCardM.href;
-// ;
-
-
-
-// linkCardM.href = linkCardMContent + cardLinkEl.href
-
-
-
-
-
-// fetch('url de back')
-//   .then(response => response.json())
-//   .then(data => {
-//       //meter href:"url" dentro del html
-//       //meter el texto entre corchetes.
-     
-
-//    cardLinkEl.href = 'url back';
-//     const newContent = document.createTextNode(cardLinkEl.href);
-//     cardLinkEl.appendChild(newContent);
-
-  
-//   });
