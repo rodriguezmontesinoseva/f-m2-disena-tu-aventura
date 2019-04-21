36e88ac413c367135f696489b5e7441b69f1886d
daniela932910 <daniela932910@gmail.com>
2019-04-21 02:36:36 +0200

    initial commit

---
+++ _src/assets/js/ls.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const inputs = document.querySelectorAll('input');
+
+for(let i = 0; i < inputs.length; i++){
+  
+    let result = inputs[i]
+    result.addEventListener('keyup', function() {
+      localStorage.setItem(result.name, result.value);
+   
+    });
+  
+       const locals = localStorage.getItem(result.name)
+       result.value = locals
+  }
+  
