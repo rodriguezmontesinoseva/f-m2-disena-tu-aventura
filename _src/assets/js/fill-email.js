ff4761a9c90a4af6ea0d59c50b20bb5dfbce6e3c
Irene Fuente <ifuenteromero@gmail.com>
2019-05-06 20:55:36 +0200

    fix email cache

--- _src/assets/js/fill-email.js
+++ _src/assets/js/fill-email.js
@@ -3,17 +3,16 @@
 const emailLink = document.querySelector('#email');
 const emailPreviewLink = document.querySelector('.email__card');
 
-// recojer el icono de la tarjeta preview de Linkdin:
+// recoger el icono de la tarjeta preview de Linkdin:
 const previewIconEmail = document.querySelector('.email-icon');
 
-function changeEmailOnCard (event){
-    const emailHref = emailLink.value;
-    
+const changeEmailOnCard = event => {
+    const emailHref = emailLink.value;  
     //Fill the userData object with this value when is selected
     const key = event.currentTarget.name;
     const value = emailHref;
     fillUserDataObject(key,value);
-        
+    saveCache();    
     // Si no hay e-mail, el href no se genera //
     if (!emailHref){
     emailPreviewLink.removeAttribute("href");
@@ -21,8 +20,6 @@ function changeEmailOnCard (event){
     // Si lo hay, se crea //
     else {
     emailPreviewLink.href = `mailto:${emailHref}`;
-
-    //llamar funccion que le añade la clase addBoxShadow__IfInputIsFIlled 
     addBoxshadowifFilled(emailLink,previewIconEmail);
     }
 }
